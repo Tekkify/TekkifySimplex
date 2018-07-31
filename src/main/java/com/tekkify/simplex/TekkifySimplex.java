1ab9a0c70d919deb87fa2a70aee635776295f9a1
LukeJS <lukejsandler@gmail.com>
2018-07-31 20:48:47 +0100

    block startup messages

--- src/main/java/com/tekkify/simplex/TekkifySimplex.java
+++ src/main/java/com/tekkify/simplex/TekkifySimplex.java
@@ -3,15 +3,37 @@ package com.tekkify.simplex;
 import com.google.common.collect.Lists;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistryModifiable;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Mod(modid = "tekkifysimplex", dependencies = "after:binniecore;")
 public class TekkifySimplex {
+
+    private static Logger logger;
+
+    private List<String> blockedMessages = Lists.newArrayList(
+            // not actually used ..
+            // ended up not needing it for now
+    );
+
+    public TekkifySimplex() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
@@ -28,6 +50,22 @@ public class TekkifySimplex {
 
         for (ResourceLocation rl : rls) {
             recipes.remove(rl);
+        }
+    }
+
+    @SubscribeEvent
+    public void onChatReceived(ClientChatReceivedEvent event) {
+        if (blockedMessages.size() == 0) {
+            return;
+        }
+
+        String plainMessage = event.getMessage().getUnformattedText().replace("\u00A7", "&");
+
+        // Blocks each message once (designed to remove messages that appear when you first load a world)
+
+        if (blockedMessages.contains(plainMessage)) {
+            blockedMessages.remove(plainMessage);
+            event.setCanceled(true);
         }
     }
 }
